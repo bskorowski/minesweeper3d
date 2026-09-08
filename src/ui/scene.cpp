40d82ccdbac0bf7ab5b248eddd1bc63a06d222a1
oosiriiss <oosiriiss@gmail.com>
2026-09-08 22:15:20 +0200

    feat: implement navigating between scenes

--- src/ui/scene.cpp
+++ src/ui/scene.cpp
@@ -2,8 +2,43 @@
 #include "debug_utils.hpp"
 #include "ui/scene.hpp"
 
-void SceneManager::addScene(std::unique_ptr<Scene> scene) {
+void SceneManager::prepareFrame() {
+
+  navigatedBack_ = false;
+  DEBUG_ASSERT(!navigatedTo_, "navigatedTo_ has not been consumed, and it is "
+                              "still present in prepareFrame()");
+  // Not needed as it should be moved from in endFrame, but prefer doing it
+  // explicitly
+  navigatedTo_ = nullptr;
+
+  DEBUG_ASSERT(!scenes_.empty(),
+               "No scene on the stack, and trying to get one.")
+  activeScene_ = scenes_.top().get();
+}
+
+void SceneManager::endFrame() {
+  activeScene_ = nullptr;
+
+  if (navigatedBack_) {
+    DEBUG_ASSERT(
+        scenes_.size() > 1 && !navigatedTo_,
+        "Couldn't navigate back to null scene, and no replacing scene was set");
+    scenes_.pop();
+  }
+  if (navigatedTo_) {
+    scenes_.emplace(std::move(navigatedTo_));
+    navigatedTo_ = nullptr; // To be explicit
+  }
+}
+
+void SceneManager::navigateTo(std::unique_ptr<Scene> scene) {
   scenes_.emplace(std::move(scene));
+}
+
+void SceneManager::navigateBack() {
+  DEBUG_ASSERT(scenes_.size() > 1,
+               "Cannot navigate back, when no scene will be active");
+  navigatedBack_ = true;
 }
 
 void SceneManager::popScene() {
@@ -12,13 +47,15 @@ void SceneManager::popScene() {
 }
 
 auto SceneManager::currentScene() -> Scene * {
-  DEBUG_ASSERT(!scenes_.empty(),
-               "No scene on the stack, and trying to get one.")
-  return scenes_.top().get();
+  DEBUG_ASSERT(
+      activeScene_,
+      "No active scene, perhaps no SceneManager::prepareFrame() was called. ");
+  return activeScene_;
 }
 
 auto SceneManager::currentScene() const -> const Scene * {
-  DEBUG_ASSERT(!scenes_.empty(),
-               "No scene on the stack, and trying to get one.")
-  return scenes_.top().get();
+  DEBUG_ASSERT(
+      activeScene_,
+      "No active scene, perhaps no SceneManager::prepareFrame() was called. ");
+  return activeScene_;
 }
--- src/ui/scene.hpp
+++ src/ui/scene.hpp
@@ -15,11 +15,25 @@ public:
 class SceneManager {
 
 public:
-  void addScene(std::unique_ptr<Scene> scene);
+  // Prepares the state for the frame
+  void prepareFrame();
+  // Cleans up state of the frame
+  void endFrame();
+  // Navigate to the scene on next frame
+  void navigateTo(std::unique_ptr<Scene> scene);
+  void navigateBack();
   void popScene();
   [[nodiscard]] auto currentScene() -> Scene *;
   [[nodiscard]] auto currentScene() const -> const Scene *;
 
 private:
+  // Scene active in this frame.
+  // Should be set via prepareFrame() at the beginning of each frame
+  Scene *activeScene_{nullptr};
   std::stack<std::unique_ptr<Scene>> scenes_;
+
+  // Whether should pop the current top scene from the stack on end of frame
+  bool navigatedBack_{false};
+  // Whether should navigate to another scene (add to stack) on end of frame
+  std::unique_ptr<Scene> navigatedTo_{nullptr};
 };
